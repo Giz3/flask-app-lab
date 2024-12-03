e917ecf18d2b6eaccbde8ac2b3225d97ed76c330
Giz3 <andrii.voitkiv.22@pnu.edu.ua>
2024-12-03 15:33:36 +0100

    Lab 3

--- app.py
+++ app.py
@@ -1,9 +1,14 @@
-from flask import Flask
+from flask import Flask, render_template
+
 app = Flask(__name__)
 
-@app.route('/')
-def main():
-    return 'Hello, world!'
+@app.route("/")
+def home():
+    return "Home page"
+
+@app.route('/resume')
+def resume():
+    return render_template('resume.html', title="Резюме")
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, port=5001)
